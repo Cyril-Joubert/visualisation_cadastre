1b82e624c5d5e42ca8ff7221f548c8b1cddf9df2
Cyril-Joubert <75437999+Cyril-Joubert@users.noreply.github.com>
2024-02-14 11:18:53 +0100

    Update streamlit_visu_cadastre.py
    add waste container map and a selectbox

--- streamlit_visu_cadastre.py
+++ streamlit_visu_cadastre.py
@@ -2,80 +2,127 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 
-column_names = ['lieu', 'lon', 'lat', "cadastre"]
-sism = pd.read_csv("sismique_grenoble_metropole.csv", sep=",", names=column_names)
+def update_map_view(selected_commune):
+    selected_data = sele[sele['commune'] == selected_commune]
+    new_view_state = pdk.ViewState(
+        latitude=selected_data['y_latitude'].mean(),
+        longitude=selected_data['x_longitude'].mean(),
+        zoom=12
+    )
+    return new_view_state
 
-inond = pd.read_csv("inondations_grenoble_metropole.csv", sep=",", names=column_names)
 
-st.write("""
-# Data visualization
-""")
+if __name__ == "__main__":
+
+    column_names = ['lieu', 'lon', 'lat', "probleme"]
+    sism = pd.read_csv("sismique_grenoble_metropole.csv", sep=",", names=column_names)
+
+    inond = pd.read_csv("inondations_grenoble_metropole.csv", sep=",", names=column_names)
+
+    sele = pd.read_csv("sele_metro.csv", sep=",")
+
+    st.write("""
+    # Data visualization
+    """)
 
 
-layer_sism = pdk.Layer(
-    'ScatterplotLayer',
-    sism,
-    get_position='[lon, lat]',
-    get_radius=50,
-    get_fill_color=[255, 0, 0],
-    pickable=True,
-    tooltip=True,
-)
+    layer_sism = pdk.Layer(
+        'ScatterplotLayer',
+        sism,
+        get_position='[lon, lat]',
+        get_radius=50,
+        get_fill_color=[255, 0, 0],
+        pickable=True,
+        tooltip=True,
+    )
 
-layer_inond = pdk.Layer(
-    'ScatterplotLayer',
-    inond,
-    get_position='[lon, lat]',
-    get_radius=50,
-    get_fill_color=[0, 0, 255],
-    pickable=True,
-    tooltip=True,
-)
+    layer_inond = pdk.Layer(
+        'ScatterplotLayer',
+        inond,
+        get_position='[lon, lat]',
+        get_radius=50,
+        get_fill_color=[0, 0, 255],
+        pickable=True,
+        tooltip=True,
+    )
 
-deck_sism = pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=pdk.ViewState(
-        latitude=sism['lat'].mean(),
-        longitude=sism['lon'].mean(),
-        zoom=9.5,
-    ),
-    layers=[layer_sism],
-    tooltip={"html": "<b>Cadastre:</b> {cadastre}", "style": {"color": "white"}}
-)
+    layer_sele = pdk.Layer(
+        'ScatterplotLayer',
+        sele,
+        get_position='[x_longitude, y_latitude]',
+        get_radius=50,
+        get_fill_color=[0, 255, 0],
+        pickable=True,
+        tooltip=True,
+    )
 
-deck_inond = pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=pdk.ViewState(
-        latitude=inond['lat'].mean(),
-        longitude=inond['lon'].mean(),
-        zoom=9.5,
-    ),
-    layers=[layer_inond],
-    tooltip={"html": "<b>Cadastre:</b> {cadastre}", "style": {"color": "white"}}
-)
 
-st.write("## Seismic issues map:")
-st.pydeck_chart(deck_sism)
+    deck_sism = pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+            latitude=sism['lat'].mean(),
+            longitude=sism['lon'].mean(),
+            zoom=9.5,
+        ),
+        layers=[layer_sism],
+        tooltip={"html": "<b>Problème:</b> {probleme}", "style": {"color": "white"}}
+    )
 
-sism_count = sism['lieu'].value_counts()
-lieu_sism = sism_count.to_frame().sort_values(by="count", ascending=False)
-st.write("### Number of issues by location:")
-st.bar_chart(lieu_sism, color=[215, 30, 30])
+    deck_inond = pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+            latitude=inond['lat'].mean(),
+            longitude=inond['lon'].mean(),
+            zoom=9.5,
+        ),
+        layers=[layer_inond],
+        tooltip={"html": "<b>Problème:</b> {probleme}", "style": {"color": "white"}}
+    )
 
-sism_type = sism['cadastre'].value_counts()
-cadastre_sism = sism_type.to_frame().sort_values(by="count", ascending=False)
-st.write("### Number of issues by type:")
-st.bar_chart(cadastre_sism, color=[215, 30, 30])
+    deck_sele = pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+            latitude=sele['y_latitude'].mean(),
+            longitude=sele['x_longitude'].mean(),
+            zoom=9.5,
+        ),
+        layers=[layer_sele],
+        tooltip={"html": "<b>Type de dechet collecté:</b> {type_dechet}<br><b>Commune: </b> {commune}", "style": {"color": "white"}}
+    )
 
-st.write("## Flood issues map:")
-st.pydeck_chart(deck_inond)
+    st.write("## Seismic issues map:")
+    st.pydeck_chart(deck_sism)
 
-inond_count = inond['lieu'].value_counts()
-lieu_inond = inond_count.to_frame().sort_values(by="count", ascending=False)
-st.write("### Number of issues by location:")
-st.bar_chart(lieu_inond, color=[0, 0, 215])
+    sism_count = sism['lieu'].value_counts()
+    lieu_sism = sism_count.to_frame().sort_values(by="count", ascending=False)
+    st.write("### Number of issues by location:")
+    st.bar_chart(lieu_sism, color=[215, 30, 30])
 
-inond_type = inond['cadastre'].value_counts()
-cadastre_inond = inond_type.to_frame().sort_values(by="count", ascending=False)
-st.write("### Number of issues by type:")
-st.bar_chart(cadastre_inond, color=[0, 0, 215])
+    sism_type = sism['probleme'].value_counts()
+    cadastre_sism = sism_type.to_frame().sort_values(by="count", ascending=False)
+    st.write("### Number of issues by type:")
+    st.bar_chart(cadastre_sism, color=[215, 30, 30])
+
+    st.write("## Flood issues map:")
+    st.pydeck_chart(deck_inond)
+
+    inond_count = inond['lieu'].value_counts()
+    lieu_inond = inond_count.to_frame().sort_values(by="count", ascending=False)
+    st.write("### Number of issues by location:")
+    st.bar_chart(lieu_inond, color=[0, 0, 215])
+
+    inond_type = inond['probleme'].value_counts()
+    cadastre_inond = inond_type.to_frame().sort_values(by="count", ascending=False)
+    st.write("### Number of issues by type:")
+    st.bar_chart(cadastre_inond, color=[0, 0, 215])
+
+    st.write("## Waste container map:")
+    selected_commune = st.selectbox('Choisir une commune : ', sele["commune"].sort_values().unique(), index=sele["commune"].sort_values().unique().tolist().index("Grenoble"))
+    view_state = update_map_view(selected_commune)
+    updated_deck_sele = pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=view_state,
+        layers=[layer_sele],
+        tooltip={"html": "<b>Type de dechet collecté:</b> {type_dechet}<br><b>Commune: </b> {commune}", "style": {"color": "white"}}
+    )
+    st.pydeck_chart(updated_deck_sele)
